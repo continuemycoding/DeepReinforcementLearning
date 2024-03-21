2a69260af436d8f141b18af9d1de39168093add9
test <test@gmail.com>
2024-03-21 10:19:16 +0800

    state.playerTurn: %d

--- funcs.py
+++ funcs.py
@@ -92,7 +92,7 @@ def playMatches(player1, player2, EPISODES, logger, turns_until_tau0, memory = N
                 memory.commit_stmemory(env.identities, state, pi)
 
 
-            logger.info('action: %d', action)
+            logger.info('state.playerTurn: %d, action: %d', state.playerTurn, action)
             for r in range(env.grid_shape[0]):
                 logger.info(['----' if x == 0 else '{0:.2f}'.format(np.round(x,2)) for x in pi[env.grid_shape[1]*r : (env.grid_shape[1]*r + env.grid_shape[1])]])
             logger.info('MCTS perceived value for %s: %f', state.pieces[str(state.playerTurn)] ,np.round(MCTS_value,2))
